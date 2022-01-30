330c39b83f4a91b907ecb1611738dcc13cbf69a4
Andreas Tasch <andy.tasch@gmail.com>
2022-01-30 22:06:18 +0100

    Updating version.

--- btcpay-greenfield-for-woocommerce.php
+++ btcpay-greenfield-for-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI:      https://btcpayserver.org
  * Text Domain:     btcpay-greenfield-for-woocommerce
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  * Requires PHP:    7.4
  * Tested up to:    5.8
  * Requires at least: 5.2
@@ -22,7 +22,7 @@ defined( 'ABSPATH' ) || exit();
 
 define( 'BTCPAYSERVER_PLUGIN_FILE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'BTCPAYSERVER_PLUGIN_URL', plugin_dir_url(__FILE__ ) );
-define( 'BTCPAYSERVER_VERSION', '0.1.4' );
+define( 'BTCPAYSERVER_VERSION', '0.1.5' );
 define( 'BTCPAYSERVER_PLUGIN_ID', 'btcpay-greenfield-for-woocommerce' );
 
 class BTCPayServerWCPlugin {
